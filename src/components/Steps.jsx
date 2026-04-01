d71d8d749d0778c8d77ece68abf0f025cfe4c2a6
SabirMCoadCraftier <mahmudcse24@gmail.com>
2026-04-02 00:19:55 +0600

    Edited

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -4,19 +4,19 @@ export default function HowItWorks() {
       id: '01',
       title: 'Create Account',
       description: 'Sign up for free in seconds. No credit card is required.',
-      image: '/user.png'
+      image:'user.png'
     },
     {
       id: '02',
       title: 'Choose Products',
       description: 'Browse our catalog and select the tools that fit your needs.',
-      image: 'package.png'
+      image:'package.png'
     },
     {
       id: '03',
       title: 'Start Creating',
       description: 'Download and start using your premium tools immediately.',
-      image: 'rocket.png'
+      image:'rocket.png'
     }
   ];
 
